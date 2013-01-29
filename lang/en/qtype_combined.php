53974467dcef26b1af4a806eb3e09ea4ca782a1d
Jamie Pratt <me@jamiep.org>
2013-01-29 16:03:12 +0700

    skeleton question type called 'combined' with ......
    countback grading
    hints
    question text
    no grading implemented at all

---
+++ lang/en/qtype_combined.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_combined', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage combined
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'Combined';
+$string['pluginname_help'] = 'Create a cloze question type with embedded response fields in the question text to enter a numeric
+or text value or select a value from a number of options.';
+$string['pluginname_link'] = 'question/type/combined';
+$string['pluginnameadding'] = 'Adding a combined question';
+$string['pluginnameediting'] = 'Editing a combined question';
+$string['pluginnamesummary'] = 'A combined question type which allows the embedding of the response fields for various available
+sub questions in the question text.
+
+So the student can enter a numeric or short text answer or choose an answer or answer(s) from
+ using a select box, check boxes or radio boxes.';
---
+++ questiontype.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Question type class for the combined question type.
+ *
+ * @package    qtype
+ * @subpackage combined
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/questionlib.php');
+require_once($CFG->dirroot . '/question/engine/lib.php');
+require_once($CFG->dirroot . '/question/type/combined/question.php');
+
+
+/**
+ * The combined question type.
+ *
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_combined extends question_type {
+
+    public function move_files($questionid, $oldcontextid, $newcontextid) {
+        parent::move_files($questionid, $oldcontextid, $newcontextid);
+        $this->move_files_in_hints($questionid, $oldcontextid, $newcontextid);
+    }
+
+    protected function delete_files($questionid, $contextid) {
+        parent::delete_files($questionid, $contextid);
+        $this->delete_files_in_hints($questionid, $contextid);
+    }
+
+    public function save_question_options($question) {
+        $this->save_hints($question);
+    }
+
+    protected function initialise_question_instance(question_definition $question, $questiondata) {
+        // TODO needs to pass through to sub-questions.
+        parent::initialise_question_instance($question, $questiondata);
+    }
+
+    public function get_random_guess_score($questiondata) {
+        // TODO needs to pass through to sub-questions.
+        return 0;
+    }
+
+    public function get_possible_responses($questiondata) {
+        // TODO needs to pass through to sub-questions.
+        return array();
+    }
+}
