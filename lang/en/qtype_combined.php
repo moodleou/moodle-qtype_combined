c2c1ebe3b1db309cec2506e64d442220d4c279c1
Tim Hunt <T.J.Hunt@open.ac.uk>
2020-02-13 17:53:28 +0000

    Combined: add missing string for multichoice subquestion #381114

--- lang/en/qtype_combined.php
+++ lang/en/qtype_combined.php
@@ -26,6 +26,7 @@
 
 $string['controlnamegapselect'] = 'drop down box';
 $string['controlnamegapselectplural'] = 'drop down boxes';
+$string['controlnamemultichoice'] = 'radio group';
 $string['correct_choice_embed_code'] = '[[{$a->qid}:{$a->qtype}:{correct choice}]]';
 $string['embeddedquestionremovedfromform'] = 'One or more embedded questions have been removed from the question text. The
 question data for these questions is still shown below, but it will be permanently removed when you either press the \'Verify the
